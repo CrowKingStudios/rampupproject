012e45a808c8aed55825c25238fac18ef5849d3e
Guy Goudeau <guy.goudeau@students.aie.edu.au>
2017-09-28 18:29:19 -0500

    Created Movement Scripts
    Created scripts for each of the player objects. They both move
    correctly, but for some reason the jump isn't working right.

---
+++ GoudeauRampUp/Assets/Scripts/MoveP2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveP2 : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 5;
+    [SerializeField]
+    private float jumpHeight = 5;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    // Prevents player from jumping while still in the air
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+            isGrounded = true;
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        isGrounded = true;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal2"), 0.0f, Input.GetAxis("Vertical2"));
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            rb.AddForce(Vector3.up * jumpHeight);
+            isGrounded = false;
+        }
+    }
+}
